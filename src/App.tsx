744d141b66542b5103f6a38f20e94db35d6ebfde
NatalyaLapusto <n.lapusto@gmail.com>
2023-09-08 14:51:23 +0300

    add modules to css

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Counter from "./components/Counter"
+import "./index.scss"
+
+const App = () => {
+    return <div className="app">
+        app
+        <Counter />
+    </div>
+}
+
+export default App
