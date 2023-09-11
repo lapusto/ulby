c1a8aaa1f542ad2f56575cd2c7786a44436e569b
NatalyaLapusto <n.lapusto@gmail.com>
2023-09-11 14:12:29 +0300

    add theme context

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./theme/ThemeProvider";
 
 import { ThemeProvider } from "./theme/ThemeProvider";
 
