a51f936aa1d44d33f2bf5c7ce8765724e8ab8ce0
NatalyaLapusto <n.lapusto@gmail.com>
2023-09-13 15:42:10 +0300

    refactor structure to FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,22 @@
 import { Link, Route, Routes } from "react-router-dom"
 import "./styles/index.scss"
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
-import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 import { Suspense } from "react"
-import { useTheme } from "./theme/useTheme"
-import { classNames } from "./helpers/classNames/classNames"
+import { classNames } from "shared/lib/classNames/classNames"
+import { useTheme } from "./providers/ThemeProvider"
+import { AboutPage } from "pages/AboutPage"
+import { MainPage } from "pages/MainPage"
 
 const App = () => {
 
- const {theme, toggleTheme} = useTheme()
+    const { theme, toggleTheme } = useTheme()
     return <div className={classNames('app', {}, [theme])}>
         <button onClick={toggleTheme}>TOGGLE THEME</button>
         <Link to={'/'}>Main</Link>
         <Link to={'/about'}>About</Link>
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route path={'/about'} element={<AboutPageAsync />} />
-                <Route path={'/'} element={<MainPageAsync />} />
+                <Route path={'/about'} element={<AboutPage />} />
+                <Route path={'/'} element={<MainPage />} />
             </Routes>
         </Suspense>
     </div>
