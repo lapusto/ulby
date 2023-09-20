fec16b647da4cd7b38234c8c8c1df0c24596cff2
NatalyaLapusto <n.lapusto@gmail.com>
2023-09-20 15:34:38 +0300

    setup bundle analyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'max-len': 'off',
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
