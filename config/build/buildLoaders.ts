db359dec10f1692e3a6c71e1a455deaef1d1a895
NatalyaLapusto <n.lapusto@gmail.com>
2023-09-25 12:26:59 +0300

    add storybook, set up storybook config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,8 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
-export function buildLoaders({ isDev, paths }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader: webpack.RuleSetRule = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
@@ -13,22 +13,7 @@ export function buildLoaders({ isDev, paths }: BuildOptions): webpack.RuleSetRul
         type: 'asset/resource',
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
-                    },
-                },
-            },
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
     const typescriptLoader = {
         test: /\.tsx?$/,
