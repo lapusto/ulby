4f865c04e0078d8de13b08e45da08e1605c4c055
NatalyaLapusto <n.lapusto@gmail.com>
2023-10-26 16:49:06 +0300

    create state

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { counterReducer } from 'entities/Counter';
 import { StateSchema } from './StateSchema';
 
-export function createReduxStore() {
+export function createReduxStore(initialState?: StateSchema) {
     return configureStore<StateSchema>({
-        reducer: {},
+        reducer: {
+            counter: counterReducer,
+        },
         devTools: __IS_DEV__,
+        preloadedState: initialState,
     });
 }
