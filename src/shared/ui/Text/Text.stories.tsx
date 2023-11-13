c99c7f87ef728f86753ece0210b45d4642117132
NatalyaLapusto <n.lapusto@gmail.com>
2023-11-13 11:10:50 +0300

    add dark theme text to storybook

--- src/shared/ui/Text/Text.stories.tsx
+++ src/shared/ui/Text/Text.stories.tsx
@@ -28,3 +28,23 @@ export const OnlyText = Template.bind({});
 OnlyText.args = {
     text: 'Lorem ipsum dolor sit amet',
 };
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+    title: 'Title',
+    text: 'Lorem ipsum dolor sit amet.',
+};
+
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OnlyTitleDark = Template.bind({});
+OnlyTitleDark.args = {
+    title: 'Title',
+};
+OnlyTitleDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OnlyTextDark = Template.bind({});
+OnlyTextDark.args = {
+    text: 'Lorem ipsum dolor sit amet',
+};
+OnlyTextDark.decorators = [ThemeDecorator(Theme.DARK)];
