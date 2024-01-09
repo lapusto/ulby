ae6e4cb378a24097a4e6cecefe3112d8d5cb0abe
NatalyaLapusto <n.lapusto@gmail.com>
2024-01-09 14:14:32 +0300

    async thunk profile data

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,8 +1,9 @@
-import { profileReducer } from 'entities/Profile';
-import { FC } from 'react';
+import { fetchProfileData, profileReducer } from 'entities/Profile';
+import { FC, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames as cn } from 'shared/lib/classNames/classNames';
 import { DynamicMuduleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicMuduleLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
 
 interface ProfilePageProps {
     className?: string;
@@ -14,6 +15,12 @@ const reducers: ReducerList = {
 
 const ProfilePage: FC<ProfilePageProps> = ({ className }) => {
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
+
     return (
         <DynamicMuduleLoader removeAfterUnmount name="profile" reducers={reducers}>
             <div className={cn('', {}, [className])}>
