8dc05463ba8180bbe27b9dcc0d54b4395b928cda
NatalyaLapusto <n.lapusto@gmail.com>
2024-01-11 13:13:37 +0300

    move state from card to profile page

--- src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -1,24 +1,32 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { classNames as cn } from 'shared/lib/classNames/classNames';
-import { useSelector } from 'react-redux';
-import { getProfileData } from 'entities/Profile/model/selectors/getProfileData/getProfileData';
-import { getProfileError } from 'entities/Profile/model/selectors/getProfileError/getProfileError';
-import { getProfileIsLoading } from 'entities/Profile/model/selectors/getProfileIsLoading/getProfileIsLoading';
+import { classNames, classNames as cn } from 'shared/lib/classNames/classNames';
 import { Text } from 'shared/ui/Text/Text';
 import { Button, ButtonVariant } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
+import { Profile } from 'entities/Profile/model/types/profile';
+import { Loader } from 'shared/ui/Loader/Loader';
 import cls from './ProfileCard.module.scss';
 
 interface ProfileCardProps {
     className?: string;
+    data?: Profile;
+    isLoading?: boolean;
+    error?: string;
 }
 
-export const ProfileCard: FC<ProfileCardProps> = ({ className }) => {
+export const ProfileCard: FC<ProfileCardProps> = ({
+    className, data, isLoading, error,
+}) => {
     const { t } = useTranslation('profile');
-    const data = useSelector(getProfileData);
-    const error = useSelector(getProfileError);
-    const isLoading = useSelector(getProfileIsLoading);
+    if (isLoading) {
+        return (
+            <div className={classNames(cls.ProfileCard, { [cls.loading]: true })}>
+                <Loader />
+            </div>
+        );
+    }
+
     return (
         <div className={cn(cls.ProfileCard, {}, [className])}>
             <div className={cls.header}>
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,6 +1,9 @@
 import { ProfileCard, fetchProfileData, profileReducer } from 'entities/Profile';
+import { getProfileData } from 'entities/Profile/model/selectors/getProfileData/getProfileData';
+import { getProfileError } from 'entities/Profile/model/selectors/getProfileError/getProfileError';
+import { getProfileIsLoading } from 'entities/Profile/model/selectors/getProfileIsLoading/getProfileIsLoading';
 import { FC, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { classNames as cn } from 'shared/lib/classNames/classNames';
 import { DynamicMuduleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicMuduleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
@@ -20,10 +23,14 @@ const ProfilePage: FC<ProfilePageProps> = ({ className }) => {
         dispatch(fetchProfileData());
     }, [dispatch]);
 
+    const data = useSelector(getProfileData);
+    const error = useSelector(getProfileError);
+    const isLoading = useSelector(getProfileIsLoading);
+
     return (
         <DynamicMuduleLoader removeAfterUnmount name="profile" reducers={reducers}>
             <div className={cn('', {}, [className])}>
-                <ProfileCard />
+                <ProfileCard data={data} isLoading={isLoading} error={error} />
             </div>
         </DynamicMuduleLoader>
 
