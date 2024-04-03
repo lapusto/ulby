45b26d72fe0893f870d8ae35a52c1f55bbd970df
NatalyaLapusto <n.lapusto@gmail.com>
2024-04-03 13:36:28 +0300

    add content to article

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -6,6 +6,9 @@ import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
 import { useSelector } from 'react-redux';
 import { Text, TextAlign, TextStyle } from 'shared/ui/Text/Text';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import CalendarIcon from 'shared/assets/icons/calendar-20-20.svg';
 import {
     getArticleDetailsData,
     getArticleDetailsError,
@@ -37,25 +40,34 @@ export const ArticleDetails: FC<ArticleDetailsProps> = ({ className, id }) => {
 
     if (isLoading) {
         content = (
-            <div>
+            <>
                 <Skeleton className={cls.avatar} width={200} height={200} border="50%" />
                 <Skeleton className={cls.title} width={300} height={32} />
                 <Skeleton className={cls.skeleton} width={600} height={24} />
                 <Skeleton className={cls.skeleton} width="100%" height={200} />
                 <Skeleton className={cls.skeleton} width="100%" height={200} />
-            </div>
+            </>
         );
     } else if (error) {
         content = (
-            <div>
-                <Text align={TextAlign.CENTER} title="Ошибка при загрузке статьи" style={TextStyle.ERROR} />
-            </div>
+            <Text align={TextAlign.CENTER} title="Ошибка при загрузке статьи" style={TextStyle.ERROR} />
         );
     } else {
         content = (
-            <div>
-                article details!
-            </div>
+            <>
+                <div className={cls.avatarWrapper}>
+                    <Avatar size={200} src={article?.img} className={cls.avatar} />
+                </div>
+                <Text title={article?.title} text={article?.subtitle} className={cls.title} />
+                <div>
+                    <EyeIcon className={cls.articleInfo} />
+                    <Text text={article?.views.toString()} />
+                </div>
+                <div>
+                    <CalendarIcon className={cls.articleInfo} />
+                    <Text text={article?.createdAt} />
+                </div>
+            </>
         );
     }
 
