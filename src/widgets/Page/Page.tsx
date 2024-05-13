db091353ffad8333b94d0d61aca6fba73e1a4ecc
Lapusto <karmesnatalia@gmail.com>
2024-05-13 12:43:20 +0300

    add useThrottle hook to Page

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -1,8 +1,15 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import {
-    memo, MutableRefObject, ReactNode, useRef,
+    memo, MutableRefObject, ReactNode, useRef, UIEvent,
+    useEffect,
 } from 'react';
 import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
+import { getSaveScrollByPath, saveScrollActions } from 'features/saveScroll';
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { useThrottle } from 'shared/lib/hooks/useThrottle';
 import cls from './Page.module.scss';
 
 interface PageProps {
@@ -15,6 +22,9 @@ export const Page = memo((props: PageProps) => {
     const { className, children, onScrollEnd } = props;
     const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
     const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const dispatch = useAppDispatch();
+    const { pathname } = useLocation();
+    const scrollPosition = useSelector((state: StateSchema) => getSaveScrollByPath(state, pathname));
 
     useInfiniteScroll({
         triggerRef,
@@ -22,10 +32,22 @@ export const Page = memo((props: PageProps) => {
         callback: onScrollEnd,
     });
 
+    useEffect(() => {
+        wrapperRef.current.scrollTop = scrollPosition;
+    }, [scrollPosition]);
+
+    const onScroll = useThrottle((e: UIEvent<HTMLDivElement>) => {
+        dispatch(saveScrollActions.setScrollPosition({
+            position: e.currentTarget.scrollTop,
+            path: pathname,
+        }));
+    }, 500);
+
     return (
         <section
             ref={wrapperRef}
             className={classNames(cls.Page, {}, [className])}
+            onScroll={onScroll}
         >
             {children}
             <div ref={triggerRef} />
